202ff3dca22dbefe6e8b402bdc23d6fc763e0961
Raja Giddi <97639175+RajaGiddi@users.noreply.github.com>
2024-07-17 01:57:40 -0400

    Update prompt message for question-answering assistant

--- app.py
+++ app.py
@@ -94,7 +94,7 @@ history_aware_retriever = create_history_aware_retriever(
 )
 
 qa_system_prompt = """You are an assistant for question-answering tasks. \
-Use the following pieces of retrieved context to answer the question. \
+Use the following pieces of retrieved context only to answer the question. \
 If you don't know the answer, just say that you don't know. \
 Use three sentences maximum and keep the answer concise.\
 
